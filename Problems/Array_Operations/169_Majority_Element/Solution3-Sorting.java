f97779a70feffd9222896a2880a149b90b7bbc48
Yilei Li <the.yilei.li@gmail.com>
2022-10-07 17:30:37 -0400

    Create Solution3-Sorting.java

---
+++ Problems/Array_Operations/169_Majority_Element/Solution3-Sorting.java
@@ -0,0 +1,18 @@
+/*
+Approach 3: Sorting
+If the elements are sorted in monotonically increasing (or decreasing) order, the majority element can be found at index n/2.
+
+Time complexity : O(nlgn)
+- Sorting the array costs O(nlgn) time in Python and Java, so it dominates the overall runtime.
+
+Space complexity : O(1) or O(n)
+- We sorted nums in place here - if that is not allowed, then we must spend linear additional space on a copy of nums and sort the copy instead.
+
+*/
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+}
