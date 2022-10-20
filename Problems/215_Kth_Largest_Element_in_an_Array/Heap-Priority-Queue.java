71d1b0f151d4675b9b9934ace5a4f2990d554661
Yilei Li <the.yilei.li@gmail.com>
2022-10-20 15:38:02 -0400

    Update Heap-Priority-Queue.java

--- Problems/215_Kth_Largest_Element_in_an_Array/Heap-Priority-Queue.java
+++ Problems/215_Kth_Largest_Element_in_an_Array/Heap-Priority-Queue.java
@@ -1,3 +1,10 @@
+/*
+The time complexity of adding an element in a heap of size k is O(logk), 
+and we do it N times that means O(Nlogk) time complexity for the algorithm.
+
+This algorithm improves time complexity, but one pays with O(k) space complexity to store the heap elements.
+*/
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         
