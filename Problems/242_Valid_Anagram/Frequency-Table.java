1c282cd753ebca2126f78e486df3cbfe780dfca0
Yilei Li <the.yilei.li@gmail.com>
2023-07-06 16:39:12 -0400

    Update Frequency-Table.java

--- Problems/242_Valid_Anagram/Frequency-Table.java
+++ Problems/242_Valid_Anagram/Frequency-Table.java
@@ -29,3 +29,22 @@ class Solution {
         return true;
     }   
 }
+
+
+// Or just do the frequency update in one go
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        
+        int arr[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+            arr[t.charAt(i)-'a']--;
+        }
+        
+        for(int test:arr)
+            if(test!=0) return false;
+        return true;
+    }
+}
